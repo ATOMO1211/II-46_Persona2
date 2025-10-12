00ad952ca1625d9e826fb4d867528569e6d3b834
karla <karlarios1294@gmail.com>
2025-10-12 15:18:35 -0600

    Progreso

--- About.aspx.vb
+++ About.aspx.vb
@@ -10,5 +10,7 @@
     Protected Sub btn_guardar_Click(sender As Object, e As EventArgs)
         persona.Nombre = txt_nombre.Text
         persona.Apellido = txt_apellido.Text
+        lbl_mensaje.Text = persona.Nombre + " " + persona.Apellido
+
     End Sub
 End Class
