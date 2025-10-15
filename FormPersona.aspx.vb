f1105584197f7745aa575f67c836e2c61b9bb72c
karla <karlarios1294@gmail.com>
2025-10-14 19:06:09 -0600

    Ultimo avanve clase 3

--- FormPersona.aspx.vb
+++ FormPersona.aspx.vb
@@ -1,11 +1,17 @@
 ﻿Public Class FormPersona
     Inherits System.Web.UI.Page
+    Public persona As New Persona()
+    Protected dbHelper As New DataBaseHelper()
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
     End Sub
 
     Protected Sub btn_guardar_Click(sender As Object, e As EventArgs)
-
+        persona.Nombre = txt_nombre.Text
+        persona.Apellido = txt_apellido.Text
+        persona.Edad = txt_edad.Text
+        lbl_mensaje.Text = persona.Nombre + " " + persona.Apellido
+        dbHelper.create(persona)
     End Sub
 End Class
